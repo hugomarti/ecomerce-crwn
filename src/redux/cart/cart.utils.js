1d3293865e17afc7474cb248898f15fa3eab521f
hugomarti <hgmarti@gmail.com>
2020-07-15 12:01:20 +0800

    Adding item to cart dropdown

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,14 @@
+export const addItemsToCart = (cartItems, cartItemsToAdd) => {
+  const existingCartItems = cartItems.find(
+    (cartItem) => cartItem.id === cartItemsToAdd.id
+  );
+  if (existingCartItems) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemsToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemsToAdd, quantity: 1 }];
+};
